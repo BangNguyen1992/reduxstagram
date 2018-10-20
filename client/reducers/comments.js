9b375c284d7e2033868d924a58c4206fd9f4737d
Bang Nguyen <bang.nguyen@lianatech.com>
2018-10-20 07:27:46 +0300

    update reducers for posts and comments

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,5 +1,31 @@
+function postComments(state = [], action) {
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      // return new state with new comment
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }]
+
+    case 'REMOVE_COMMENT':
+      // return new state without delete comment
+      return [
+        ...state.slice(0, action.index),
+        ...state.slice(action.index + 1)
+      ]
+
+    default:
+      return state;
+  }
+}
+
 function comments(state = [], action) {
-  console.log('object comments', state, action);
+  if (typeof action.postId !== 'undefined') {
+    return {
+      ...state, //take the current state
+      [action.postId]: postComments(state[action.postId], action) // overwrite the post with new one
+    }
+  }
   return state;
 }
 
