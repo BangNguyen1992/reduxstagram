4d2390c95f4984b9a4fd7a3feb9ba82a9075e6a9
Bang Nguyen <bang.nguyen@lianatech.com>
2018-10-20 03:51:50 +0300

    1st working stage

---
+++ webpack.config.dev.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './client/reduxstagram'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  module: {
+    rules: [
+      // js
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader'],
+        include: path.join(__dirname, 'client'),
+        exclude: /node_modules/
+      },
+      // CSS
+      {
+        test: /\.styl$/,
+        include: path.join(__dirname, 'client'),
+        loader: 'style-loader!css-loader!stylus-loader'
+      }
+    ]
+  }
+};
